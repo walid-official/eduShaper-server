0030a5c8bbc1196754907214711e05ef22281a0d
walid hasan <walid.official8@gmail.com>
2024-12-22 23:33:26 +0600

    delete operation is added

--- index.js
+++ index.js
@@ -61,6 +61,12 @@ async function run() {
       res.send(result)
     });
 
+    app.delete("/deleteService/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await eduServiceCollection.deleteOne(query);
+      res.send(result)
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
