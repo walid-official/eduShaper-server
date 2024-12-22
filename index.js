9c0277c80658acf58e88e82dee6003a9fee819e4
walid hasan <walid.official8@gmail.com>
2024-12-22 21:31:33 +0600

    get operation is done with id and all services

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000
 const app = express()
@@ -42,8 +42,17 @@ async function run() {
       res.send(result);
     })
 
+    app.get("/services", async (req, res) => {
+      const result = await eduServiceCollection.find().toArray();
+      res.send(result)
+    });
 
-
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await eduServiceCollection.findOne(query);
+      res.send(result)
+    });
 
 
     // Send a ping to confirm a successful connection
