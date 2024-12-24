038a2252bbfa62780fa49516d1d5dd875d713241
walid hasan <walid.official8@gmail.com>
2024-12-24 12:57:29 +0600

    server is deployed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 var jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
@@ -9,39 +9,37 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5174",
+      "https://edu-service-sharing-web.vercel.app",
+      "https://edu-service-sharing-c50kvndcj-walid-hasans-projects.vercel.app",
+      "https://education-service-d2fdb.web.app"
+    ],
     credentials: true,
   })
 );
 app.use(express.json());
-app.use(cookieParser())
-
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello from SoloSphere Server....");
 });
 
-const verifyToken = (req,res,next) => {
+const verifyToken = (req, res, next) => {
   console.log("Inside the Logger", req.cookies);
   const token = req.cookies?.token;
   console.log(token);
-  if(!token){
-    return res.status(401).send({message: "Unauthorized access"})
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized access" });
   }
-  jwt.verify(token,process.env.ACCESS_SECRET_KEY,(err,decoded) => {
-    if(err){
-      return res.status(401).send({message: "Unauthorized access"})
+  jwt.verify(token, process.env.ACCESS_SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized access" });
     }
     req.user = decoded;
     next();
-  })
-
-}
-
-
-
-
-
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pxdhv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -57,15 +55,12 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
-
-    
     const eduServiceCollection = client.db("edu_db").collection("services");
     const bookServiceCollection = client
       .db("bookService_db")
       .collection("bookServices");
-
 
     // Auth related ApIs
 
@@ -77,16 +72,20 @@ async function run() {
       res
         .cookie("token", token, {
           httpOnly: true,
-          secure: false,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         })
         .send({ success: true });
     });
 
     app.post("/logout", async (req, res) => {
-      res.clearCookie("token",{
-        httpOnly: true,
-        secure: false,
-      }).send({ success: true });
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
     });
 
     app.post("/addService", async (req, res) => {
@@ -103,8 +102,8 @@ async function run() {
     app.get("/services/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
       const query = { "serviceProviderData.email": email };
-      if(req.user.email !== email){
-        return res.status(403).send({message: "Forbidden Access"})
+      if (req.user.email !== email) {
+        return res.status(403).send({ message: "Forbidden Access" });
       }
       // const verify = req.user.email;
       // const verify2 = email;
@@ -155,18 +154,23 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/bookServices/:email", async (req, res) => {
+    app.get("/bookServices/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
-
       const query = { currentUserEmail: email };
+      if (req.user.email !== email) {
+        return res.status(403).send({ message: "Forbidden Access" });
+      }
 
       const result = await bookServiceCollection.find(query).toArray();
       res.send(result);
     });
 
-    app.get("/bookToDoServices/:email", async (req, res) => {
+    app.get("/bookToDoServices/:email",verifyToken, async (req, res) => {
       const email = req.params.email;
       const query = { providerEmail: email };
+      if (req.user.email !== email) {
+        return res.status(403).send({ message: "Forbidden Access" });
+      }
       const result = await bookServiceCollection.find(query).toArray();
       res.send(result);
     });
@@ -185,7 +189,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
